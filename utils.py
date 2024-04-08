324fb34eba30d4eedc95a10288e9a79e80f1f861
cgudivad <chetana.gudivada@valpo.edu>
2024-04-08 16:56:59 -0500

    Updates on index.html: 1. Modified OpenAI Logo 2. Made GPT 4 as the default model Updates on requirements.txt: 1. Added new dependency youtube-transcript-api Updates on utils.py 1. Modified logic for fetching transcripts from rapid api to youtube-transcript-api 2. Added logic for supporting multiple languages 3. Added auto translation logic from any language to english

--- utils.py
+++ utils.py
@@ -1,7 +1,5 @@
 import os
 from dotenv import load_dotenv
-import requests
-import re
 from langchain_openai.chat_models import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain.prompts import ChatPromptTemplate
@@ -11,6 +9,7 @@ from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_core.runnables import RunnablePassthrough
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
+from youtube_transcript_api import YouTubeTranscriptApi
 
 load_dotenv()
 
@@ -32,6 +31,59 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20
 embeddings = OpenAIEmbeddings(model=EMBEDDINGS_MODEL)
 model_map = {"gpt-3.5-turbo": 5, "gpt-4-turbo-preview": 10}
 
+def get_video_id(url):
+    if "youtube.com/watch" in url:
+        video_id = url.split("youtube.com/watch?v=")[-1].split("&")[0]
+    elif "youtube.com/live" in url:
+        video_id = url.split("youtube.com/live/")[1].split("?")[0]
+    elif "youtu.be/" in url:
+        video_id = url.split("youtu.be/")[1].split("?")[0]
+    else:
+        video_id = url
+    return video_id
+
+def get_name_space(video_id):
+    return video_id.lower().replace("_", "-")
+
+def get_name_spaces():
+    return Pinecone().Index(PINECONE_INDEX_NAME).describe_index_stats()['namespaces']
+
+def get_transcript(video_id):
+
+    transcript = None
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    except Exception as e:
+        try:
+            transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+            for transcript_obj in transcript_list:
+                if transcript_obj.is_translatable:
+                    try:
+                        transcript = transcript_obj.translate('en').fetch()
+                        break
+                    except Exception as e:
+                        print(e)
+        except Exception as e:
+            print(e)
+
+    if transcript is None:
+        return ("error", "No transcript found")
+    return ("success", ' '.join(item['text'] for item in transcript))
+
+def upsert_transcript(data, url, name_space):
+    try:
+        doc = Document(
+                page_content=data,
+                metadata={"source": url}
+            )
+        documents = text_splitter.split_documents([doc])
+        PineconeVectorStore.from_documents(
+            documents, embeddings, index_name=PINECONE_INDEX_NAME, namespace=name_space
+        )
+    except Exception as e:
+        return ("error", e)
+    return ("success", name_space)
+
 def load(url):
     video_id = get_video_id(url)
     name_space = get_name_space(video_id)
@@ -51,57 +103,6 @@ def load(url):
 
     return ("success", name_space)
 
-def get_video_id(url):
-    if "youtube.com/watch" in url:
-        video_id = url.split("youtube.com/watch?v=")[-1].split("&")[0]
-    elif "youtube.com/live" in url:
-        video_id = url.split("youtube.com/live/")[1].split("?")[0]
-    elif "youtu.be/" in url:
-        video_id = url.split("youtu.be/")[1].split("?")[0]
-    else:
-        video_id = url
-    return video_id
-
-def get_name_space(video_id):
-    return video_id.lower().replace("_", "-")
-
-def get_name_spaces():
-    return Pinecone().Index(PINECONE_INDEX_NAME).describe_index_stats()['namespaces']
-
-def get_transcript(video_id):
-    querystring = {"videoId":video_id, "lang":"en", "format":"text"}
-
-    headers = {
-        "X-RapidAPI-Key": YT_API_KEY,
-        "X-RapidAPI-Host": "youtube-captions-and-transcripts.p.rapidapi.com"
-    }
-
-    response = requests.get(YT_API_URL, headers=headers, params=querystring)
-    if response.status_code != 200:
-        return ("error", response.json()['message'])
-    response_body = response.json()
-    if response_body['status'] != 200:
-        return ("error", "Transcript not found. Please try Another Video.")
-    response_data = response_body['data']
-    output_str = re.sub(r'\n', ' ', response_data)
-    output_str = re.sub(r'(\[.*?\]\s?|‑‑|\xa0)', '', output_str)
-
-    return ("success", output_str)
-
-def upsert_transcript(data, url, name_space):
-    try:
-        doc = Document(
-                page_content=data,
-                metadata={"source": url}
-            )
-        documents = text_splitter.split_documents([doc])
-        PineconeVectorStore.from_documents(
-            documents, embeddings, index_name=PINECONE_INDEX_NAME, namespace=name_space
-        )
-    except Exception as e:
-        return ("error", e)
-    return ("success", name_space)
-
 def generate(model, name_space, question):
     model_obj = ChatOpenAI(model=model, streaming=True)
     pinecone = PineconeVectorStore(
@@ -116,32 +117,17 @@ def generate(model, name_space, question):
 
     return chain.stream(question)
 
-def generate_response(model, name_space, question):
-    model_obj = ChatOpenAI(model=model, streaming=True)
-    pinecone = PineconeVectorStore(
-            embedding=embeddings, index_name=PINECONE_INDEX_NAME, namespace=name_space
-        )
-    chain = (
-    {"context": pinecone.as_retriever(k=model_map[model]), "question": RunnablePassthrough()}
-        | prompt
-        | model_obj
-        | parser
-    )
-
-    return chain.invoke(question)
-
 
 if __name__ == "__main__":
-    import time
+    # import time
     # start_time = time.time()
     # status, name_space = load("https://www.youtube.com/watch?v=kfrbkm_nmak")
     # end_time = time.time()
     # print("load Elapsed time:", end_time - start_time, "seconds")
-    start_time = time.time()
-    for chunk in generate("gpt-4-turbo-preview", get_name_space("java-interview-questions"), "what is the life cycle of servlet?"):
-        print(chunk)
-    print(generate_response("gpt-4-turbo-preview", get_name_space("java-interview-questions"), "what is the life cycle of servlet?"))
-    end_time = time.time()
-    print("generate Elapsed time:", end_time - start_time, "seconds")
+    # start_time = time.time()
+    # for chunk in generate("gpt-4-turbo-preview", get_name_space("java-interview-questions"), "what is the life cycle of servlet?"):
+    #     print(chunk)
+    # end_time = time.time()
+    # print("generate Elapsed time:", end_time - start_time, "seconds")
     pass
     
